d12cafc6cccbce91bf0c3ae4d0c49986f14ebfe5
elrado <radoosredkar@hotmail.com>
2020-04-17 14:28:59 +0200

    Rest running and data is there

---
+++ src/main/java/com/ineor/vat/VatApplication.java
@@ -0,0 +1,13 @@
+package com.ineor.vat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ineor/vat/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ineor.vat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author rado
+ */
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(2, String.format(template, name));
+	}
+}
